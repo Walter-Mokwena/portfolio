c7a125d349f56b2bec5dbee37ea20615cbf6628e
Walter Mokwena <mokwenawalter50@gmail.com>
2026-02-24 15:04:44 +0000

    updated website

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function scrollToProjects() {
     document.getElementById("projects")
         .scrollIntoView({ behavior: "smooth" });
 }
+
+function moveSlide(index) {
+    const wrapper = document.getElementById('carouselWrapper');
+    const dots = document.querySelectorAll('.dot');
+    
+    // Move the wrapper by -100%, -200%, etc.
+    wrapper.style.transform = `translateX(-${index * 100}%)`;
+    
+    // Update Active Dot
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
